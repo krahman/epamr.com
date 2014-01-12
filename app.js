ba7107a2fafd76889fd967e647d7732baec67350
khal <khal.rahman@gmail.com>
2014-01-12 18:46:29 +0800

    adding mongodb config to the project

--- app.js
+++ app.js
@@ -3,11 +3,20 @@
  * Module dependencies.
  */
 
-var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var http = require('http');
-var path = require('path');
+var express = require('express')
+    ,routes = require('./routes')
+    ,user = require('./routes/user')
+    ,http = require('http')
+    ,path = require('path');
+
+// load configuration
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+// Initializing database
+var config = require('./config/config')
+    ,mongoose = require('mongoose');
+
+var db = mongoose.connect(config.db);
 
 var app = express();
 
